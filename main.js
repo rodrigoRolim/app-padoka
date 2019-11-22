dbc155a0d7c995675165de5b89e5fac38e03d21b
Rodrigo Rolim <rodrigo@codemed.com.br>
2019-11-22 16:50:42 -0300

    init

---
+++ main.js
@@ -0,0 +1,41 @@
+const electron = require('electron')
+const path = require('path')
+const BrowserWindow = electron.BrowserWindow
+const app = electron.app
+const globalShortcut = electron.globalShortcut
+
+let mainWindow;
+
+function createWindow () {
+  globalShortcut.register('f5', function() {
+		console.log('f5 is pressed')
+		mainWindow.reload()
+	})
+  mainWindow = new BrowserWindow({autoHideMenuBar: true,
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  mainWindow.removeMenu();
+  mainWindow.loadFile('index.html')
+  mainWindow.webContents.openDevTools()
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+app.on('window-all-closed', () => {
+
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
